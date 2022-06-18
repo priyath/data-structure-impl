141aa88fb804c2e01a75f04974904cbc63cef5f4
priyath <priyath.gregory@gmail.com>
2022-06-19 00:08:34 +0800

    add to index method added

--- SingleLinkedList.js
+++ SingleLinkedList.js
@@ -32,6 +32,41 @@ class SingleLinkedList {
         this.size = this.size + 1;
     }
 
+    get(index) {
+        let counter = 0;
+        let targetNode = this.head;
+
+        while(counter < index) {
+            targetNode = targetNode.next;
+            counter++;
+        }
+        return targetNode;
+    }
+
+    addToIndex(item, index) {
+        if (index < 0 || index >= this.size) {
+            console.error('Invalid index');
+            return false;
+        }
+
+        const newNode = new Node(item);
+
+        if (index === 0) {
+            const temp = this.head;
+            this.head = newNode;
+            this.head.next = temp;
+            return true;
+        }
+
+        const targetNode = this.get(index);
+        const previousNode = this.get(index-1);
+
+        previousNode.next = newNode;
+        newNode.next = targetNode;
+
+        return true;
+    }
+
     print() {
         let nextNode = this.head;
         while (nextNode) {
@@ -42,7 +77,15 @@ class SingleLinkedList {
 }
 
 const list = new SingleLinkedList();
+list.addToIndex(999, 0);
 list.add(1);
 list.add(2);
 list.add(3);
+list.add(5);
+list.print();
+console.log('----------------------------');
+list.addToIndex(4, 3);
+list.print();
+console.log('----------------------------');
+list.addToIndex(0, -1);
 list.print();
